2305da0ddf7530404733fd1e356d1d68ea55e0b9
bu2terfly <121181079+bu2terfly@users.noreply.github.com>
2024-10-18 11:43:34 +0530

    Update prefix_&_suffix.py

--- plugins/prefix_&_suffix.py
+++ plugins/prefix_&_suffix.py
@@ -11,16 +11,16 @@ async def add_prefix(client, message):
     prefix = message.text.split(" ", 1)[1]
     RknDev = await message.reply_text("**ᴘʀᴏᴄᴇssɪɴɢ.....**", reply_to_message_id=message.id)
     await db.set_prefix(message.from_user.id, prefix)
-    await RknDev.edit("**ᴘʀᴇғғɪx  sᴀᴠᴇᴅ, ᴇᴅɪᴛ  ᴀ  ғɪʟᴇ  ᴛᴏ  sᴇᴇ** ☑️\n\n#ad\nreserve for ad")
+    await RknDev.edit("**ᴘʀᴇғғɪx  sᴀᴠᴇᴅ, ᴇᴅɪᴛ  ᴀ  ғɪʟᴇ  ᴛᴏ  sᴇᴇ** ☑️")
 
 @Client.on_message(filters.private & filters.command('del_prefix'))
 async def delete_prefix(client, message):
     RknDev = await message.reply_text("**ᴘʀᴏᴄᴇssɪɴɢ.....**", reply_to_message_id=message.id)
     prefix = await db.get_prefix(message.from_user.id)
     if not prefix:
-        return await RknDev.edit("**✖️ɴᴏ  ᴘʀᴇғғɪx  sᴀᴠᴇᴅ, ғɪʀsᴛ  sᴇᴛ  ᴀ  ᴘʀᴇғɪx**\n\n#ad\nreserve for ad")
+        return await RknDev.edit("**✖️ɴᴏ  ᴘʀᴇғғɪx  sᴀᴠᴇᴅ, ғɪʀsᴛ  sᴇᴛ  ᴀ  ᴘʀᴇғɪx**")
     await db.set_prefix(message.from_user.id, None)
-    await RknDev.edit("**ᴘʀᴇғғɪx  ᴅᴇʟᴇᴛᴇᴅ🚫, sᴇᴛ  ᴀ  ɴᴇᴡ  ᴏɴᴇ**\n\n#ad\nreserve for ad")
+    await RknDev.edit("**ᴘʀᴇғғɪx  ᴅᴇʟᴇᴛᴇᴅ🚫, sᴇᴛ  ᴀ  ɴᴇᴡ  ᴏɴᴇ**")
 
 @Client.on_message(filters.private & filters.command('see_prefix'))
 async def see_prefix(client, message):
@@ -29,7 +29,7 @@ async def see_prefix(client, message):
     if prefix:
         await RknDev.edit(f"**ʏᴏᴜʀ ᴘʀᴇꜰɪx:-**\n\n`{prefix}`")
     else:
-        await RknDev.edit("**✖️ɴᴏ  ᴘʀᴇғғɪx  sᴀᴠᴇᴅ, ғɪʀsᴛ  sᴇᴛ  ᴀ  ᴘʀᴇғɪx**\n\n#ad\nreserve for ad")
+        await RknDev.edit("**✖️ɴᴏ  ᴘʀᴇғғɪx  sᴀᴠᴇᴅ, ғɪʀsᴛ  sᴇᴛ  ᴀ  ᴘʀᴇғɪx**")
 
 # SUFFIX COMMOND ✨
 @Client.on_message(filters.private & filters.command('set_suffix'))
@@ -39,16 +39,16 @@ async def add_suffix(client, message):
     suffix = message.text.split(" ", 1)[1]
     RknDev = await message.reply_text("**ᴘʀᴏᴄᴇssɪɴɢ.....**", reply_to_message_id=message.id)
     await db.set_suffix(message.from_user.id, suffix)
-    await RknDev.edit("**sᴜғғɪx  sᴀᴠᴇᴅ, ᴇᴅɪᴛ  ᴀ  ғɪʟᴇ  ᴛᴏ  sᴇᴇ** ☑️\n\n#ad\nreserve for ad")
+    await RknDev.edit("**sᴜғғɪx  sᴀᴠᴇᴅ, ᴇᴅɪᴛ  ᴀ  ғɪʟᴇ  ᴛᴏ  sᴇᴇ** ☑️")
 
 @Client.on_message(filters.private & filters.command('del_suffix'))
 async def delete_suffix(client, message):
     RknDev = await message.reply_text("**ᴘʀᴏᴄᴇssɪɴɢ......**", reply_to_message_id=message.id)
     suffix = await db.get_suffix(message.from_user.id)
     if not suffix:
-        return await RknDev.edit("**✖️ɴᴏ  sᴜғғɪx  sᴀᴠᴇᴅ, ғɪʀsᴛ  sᴇᴛ  ᴀ  sᴜғғɪx**\n\n#ad\nreserve for ad")
+        return await RknDev.edit("**✖️ɴᴏ  sᴜғғɪx  sᴀᴠᴇᴅ, ғɪʀsᴛ  sᴇᴛ  ᴀ  sᴜғғɪx**")
     await db.set_suffix(message.from_user.id, None)
-    await RknDev.edit("**sᴜғғɪx  ᴅᴇʟᴇᴛᴇᴅ🚫, sᴇᴛ  ᴀ  ɴᴇᴡ  ᴏɴᴇ**\n\n#ad\nreserve for ad")
+    await RknDev.edit("**sᴜғғɪx  ᴅᴇʟᴇᴛᴇᴅ🚫, sᴇᴛ  ᴀ  ɴᴇᴡ  ᴏɴᴇ**")
 
 @Client.on_message(filters.private & filters.command('see_suffix'))
 async def see_suffix(client, message):
@@ -57,5 +57,5 @@ async def see_suffix(client, message):
     if suffix:
         await RknDev.edit(f"**ᴄᴜʀʀᴇɴᴛ  ꜱᴜꜰꜰɪx:-**\n\n`{suffix}`")
     else:
-        await RknDev.edit("**✖️ɴᴏ  sᴜғғɪx  sᴀᴠᴇᴅ, ғɪʀsᴛ  sᴇᴛ  ᴀ  sᴜғғɪx**\n\n#ad\nreserve for ad**")
+        await RknDev.edit("**✖️ɴᴏ  sᴜғғɪx  sᴀᴠᴇᴅ, ғɪʀsᴛ  sᴇᴛ  ᴀ  sᴜғғɪx**")
 
