ee8f0d25a2e3537634ef8a335f36f78802ecc57f
bu2terfly <121181079+bu2terfly@users.noreply.github.com>
2024-11-05 00:50:37 +0530

    Update start_&_cb.py

--- plugins/start_&_cb.py
+++ plugins/start_&_cb.py
@@ -39,7 +39,7 @@ async def start(client, message):
     else:
         await message.reply_text(text=caption, reply_markup=start_button, disable_web_page_preview=True)
     
-    await asyncio.sleep(9)
+    await asyncio.sleep(90)
     
     second_caption = rkn.START_TXT2.format(user.mention)
     
